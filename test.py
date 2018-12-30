89de3ef5052c477a23f6a2ff967e936bc866b7f3
marrowleaves <marrowleaves@gmail.com>
2018-12-30 12:32:46 +0800

    seperate config information from code

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
-# create a py file, to open baidu and do search function    
-
-import os
 import time
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import unittest
+from lucyutils.config import Config, DRIVER_PATH
 
 class TestBaidu(unittest.TestCase):
 
-    URL = "http://www.baidu.com"
-    base_path = os.path.dirname(os.path.abspath(__file__)) + '\..'
-    driver_path = os.path.abspath(base_path+'\drivers\chromedriver.exe')
+    URL = Config().get('URL') # 1：从Config中读取url
 
     locator_kw = (By.ID, 'kw')
     locator_su = (By.ID, 'su')
     locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
 
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=self.driver_path)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH + '\chromedriver.exe') # 2：从Config中读取path
         self.driver.get(self.URL)
 
     def tearDown(self):
@@ -30,5 +26,14 @@ class TestBaidu(unittest.TestCase):
         links = self.driver.find_elements(*self.locator_result)
         for link in links:
             print(link.text)
+
+    def test_search_1(self):
+        self.driver.find_element(*self.locator_kw).send_keys('selenium 测试 2')
+        self.driver.find_element(*self.locator_su).click()
+        time.sleep(2)
+        links = self.driver.find_elements(*self.locator_result)
+        for link in links:
+            print(link.text)
+
 if __name__ == '__main__':
     unittest.main()

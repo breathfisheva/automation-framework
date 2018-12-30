e5958ac3bf927398b6e3a693a260eeac91104fdc
breathfisheva <>
2018-12-30 14:11:06 +0800

    use pageobject module

--- test/case/test_baidu.py
+++ test/case/test_baidu.py
@@ -1,37 +1,34 @@
 import time
 import unittest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from lucyutils.config import Config, DRIVER_PATH, DATA_PATH, REPORT_PATH
+from lucyutils.config import Config, DATA_PATH, REPORT_PATH
 from lucyutils.log import logger
 from lucyutils.file_reader import ExcelReader
 from lucyutils.htmltestrunner import HTMLTestRunner
-
+from test.page.baidu_result_page import BaiDuMainPage, BaiDuResultPage
 
 class TestBaiDu(unittest.TestCase):
     URL = Config().get('URL')
     excel = DATA_PATH + '/baidu.xlsx'
 
-    locator_kw = (By.ID, 'kw')
-    locator_su = (By.ID, 'su')
-    locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
-
     def sub_setUp(self):
-        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH + '\chromedriver.exe')
-        self.driver.get(self.URL)
+        # 初始页面是main page，传入浏览器类型打开浏览器
+        self.page = BaiDuMainPage(browser_type='chrome').get(self.URL, maximize_window=False)
 
     def sub_tearDown(self):
-        self.driver.quit()
+        self.page.quit() #注意这里不是self.driver，这里TestBaiDu类以及没有driver了
 
     def test_search(self):
         datas = ExcelReader(self.excel).data
         for d in datas:
             with self.subTest(data=d):
                 self.sub_setUp()
-                self.driver.find_element(*self.locator_kw).send_keys(d['search'])
-                self.driver.find_element(*self.locator_su).click()
+                # self.driver.find_element(*self.locator_kw).send_keys(d['search'])
+                # self.driver.find_element(*self.locator_su).click()
+                self.page.search(d['search'])
                 time.sleep(2)
-                links = self.driver.find_elements(*self.locator_result)
+                # links = self.driver.find_elements(*self.locator_result)
+                self.page = BaiDuResultPage(self.page)  # 页面跳转到result page
+                links = self.page.result_links
                 for link in links:
                     logger.info(link.text)
                 self.sub_tearDown()

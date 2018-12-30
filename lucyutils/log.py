1e4d305d1faf8cd5c7fb9f3234d07fe5f064c6c4
marrowleaves <marrowleaves@gmail.com>
2018-12-30 13:07:37 +0800

    seperate config info from log.py

--- lucyutils/log.py
+++ lucyutils/log.py
@@ -1,23 +1,34 @@
+"""
+日志类。通过读取配置文件，定义日志级别、日志文件名、日志格式等。
+一般直接把logger import进去
+from utils.log import logger
+logger.info('test log')
+"""
+import os
 import logging
 from logging.handlers import TimedRotatingFileHandler
-from lucyutils.config import LOG_PATH
-import os
+from lucyutils.config import LOG_PATH, Config
 
 
 class Logger(object):
     def __init__(self, logger_name='framework'):
         self.logger = logging.getLogger(logger_name)
         logging.root.setLevel(logging.NOTSET)
-        self.log_file_name = 'test.log'
-        self.backup_count = 5
+        c = Config().get('log')
+        self.log_file_name = c.get('file_name') if c and c.get('file_name') else 'test.log'  # 日志文件
+        self.backup_count = c.get('backup') if c and c.get('backup') else 5  # 保留的日志数量
         # 日志输出级别
-        self.console_output_level = 'WARNING'
-        self.file_output_level = 'DEBUG'
+        self.console_output_level = c.get('console_level') if c and c.get('console_level') else 'WARNING'
+        self.file_output_level = c.get('file_level') if c and c.get('file_level') else 'DEBUG'
         # 日志输出格式
-        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        pattern = c.get('pattern') if c and c.get('pattern') else '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        self.formatter = logging.Formatter(pattern)
 
     def get_logger(self):
-        """在logger中添加日志句柄并返回，如果logger已有句柄，则直接返回"""
+        """在logger中添加日志句柄并返回，如果logger已有句柄，则直接返回
+        我们这里添加两个句柄，一个输出日志到控制台，另一个输出到日志文件。
+        两个句柄的日志级别不同，在配置文件中可设置。
+        """
         if not self.logger.handlers:  # 避免重复日志
             console_handler = logging.StreamHandler()
             console_handler.setFormatter(self.formatter)
@@ -25,7 +36,7 @@ class Logger(object):
             self.logger.addHandler(console_handler)
 
             # 每天重新创建一个日志文件，最多保留backup_count份
-            file_handler = TimedRotatingFileHandler(filename=os.path.join(LOG_PATH,self.log_file_name),
+            file_handler = TimedRotatingFileHandler(filename=os.path.join(LOG_PATH, self.log_file_name),
                                                     when='D',
                                                     interval=1,
                                                     backupCount=self.backup_count,

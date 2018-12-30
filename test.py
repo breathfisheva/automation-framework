b69c8589d92dee489914710e4796688c8dd6c86a
marrowleaves <marrowleaves@gmail.com>
2018-12-30 12:54:47 +0800

    add log function

--- test.py
+++ test.py
@@ -3,17 +3,18 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from lucyutils.config import Config, DRIVER_PATH
+from lucyutils.log import logger
 
-class TestBaidu(unittest.TestCase):
 
-    URL = Config().get('URL') # 1：从Config中读取url
+class TestBaiDu(unittest.TestCase):
+    URL = Config().get('URL')
 
     locator_kw = (By.ID, 'kw')
     locator_su = (By.ID, 'su')
     locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
 
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH + '\chromedriver.exe') # 2：从Config中读取path
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH + '\chromedriver.exe')
         self.driver.get(self.URL)
 
     def tearDown(self):
@@ -25,15 +26,16 @@ class TestBaidu(unittest.TestCase):
         time.sleep(2)
         links = self.driver.find_elements(*self.locator_result)
         for link in links:
-            print(link.text)
+            logger.info(link.text)
 
     def test_search_1(self):
-        self.driver.find_element(*self.locator_kw).send_keys('selenium 测试 2')
+        self.driver.find_element(*self.locator_kw).send_keys('Python selenium')
         self.driver.find_element(*self.locator_su).click()
         time.sleep(2)
         links = self.driver.find_elements(*self.locator_result)
         for link in links:
-            print(link.text)
+            logger.info(link.text)
+
 
 if __name__ == '__main__':
     unittest.main()
